b0e957f1a53ff5f57bb60ace82bf36fb8e320c82
ssh891123 <ssh891123@gmail.com>
2023-06-07 22:47:42 +0900

    [feat] 로드맵 함수화
    - 카카오 맵 가져오는 로직을 함수처리

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,15 @@ import './App.css';
 // typescript는 정의되지 않은 type은 에러를 발생함
 declare global {
   interface Window {
-    kakao: any
+    loadMap: () => void
   }
 }
 
 function App() {
 
+  // loadMap은 useEffect 안에서 호출 => 처음 렌더링될 때만 map을 가져오기 위함 
   useEffect(()=>{
-    // https://apis.map.kakao.com/web/guide/
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-
-    const map = new window.kakao.maps.Map(container, options);
+    window.loadMap();
   }, []);
 
   return (
