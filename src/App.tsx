66ba9418dea927deb31182cdded26dfc24d45fc8
ssh891123 <ssh891123@gmail.com>
2023-06-07 23:13:14 +0900

    [feat] 카카오 map을 동적으로 가져오도록 추가
    - index.html에 대신 app.tsx가 렌더링이 되면 카카오 맵을 가져오도록 변경
    -- index.html에 추가하면 항상 맵을 가져오는 방식이라 비추천

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 // typescript는 정의되지 않은 type은 에러를 발생함
 declare global {
   interface Window {
-    loadMap: () => void
+    kakao: any;
   }
 }
 
 function App() {
+  const mapRef = useRef<HTMLDivElement>(null);
 
   // loadMap은 useEffect 안에서 호출 => 처음 렌더링될 때만 map을 가져오기 위함 
   useEffect(()=>{
-    window.loadMap();
+    const script = document.createElement('script');
+    // autoload=false | 동적으로 로딩하고 페이지에 맵을 삽입하기 위함
+    // ??? | 카카오 개발자 사이트에서 앱 키 > javascript 키로 대체
+    script.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=???&autoload=false';
+
+    document.head.appendChild(script);
+    script.onload = () => {
+      if(mapRef.current) {
+        // https://apis.map.kakao.com/web/documentation/
+        // script가 load되어도 map이 완료되지 않을 수 있음
+        window.kakao.maps.load(()=> {
+          // https://apis.map.kakao.com/web/guide/
+          const options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+          };
+          const map = new window.kakao.maps.Map(mapRef.current, options);
+          })
+      }
+    }
+
+    // unmount(제거)가 될 때, script도 제거
+    return () => script.remove();
   }, []);
+  
 
   return (
     <div>
-      <div id= 'map'
-      style={{
-        width:500,
-        height:500,
-      }}></div>
+      <div
+        ref={mapRef}
+        style={{
+          width:500,
+          height:500,
+        }}></div>
     </div>
   );
 }
