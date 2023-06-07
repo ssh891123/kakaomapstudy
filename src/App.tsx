5d453ec03d8ff0c7aac6c52b42b7513206683a66
ssh891123 <ssh891123@gmail.com>
2023-06-07 22:38:58 +0900

    [first] 카카오 지도 띄우기
    - 웹에 카카오 지도 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// typescript는 정의되지 않은 type은 에러를 발생함
+declare global {
+  interface Window {
+    kakao: any
+  }
+}
+
 function App() {
+
+  useEffect(()=>{
+    // https://apis.map.kakao.com/web/guide/
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+
+    const options = { //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new window.kakao.maps.Map(container, options);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id= 'map'
+      style={{
+        width:500,
+        height:500,
+      }}></div>
     </div>
   );
 }
